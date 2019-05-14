784c9d94dc72d6b48fada5783790311a3c004f64
紫密 <mengchen.cmc@alibaba-inc.com>
2019-05-14 22:06:29 +0800

    revert transition

---
+++ claripy/utils/transition.py
@@ -0,0 +1,9 @@
+import sys
+
+PY2 = str is bytes
+
+def raise_from(new_error, old_error):
+    if PY2:
+        exec('raise type(new_error), new_error, sys.exc_info()[2]')
+    else:
+        exec('raise new_error from old_error')
